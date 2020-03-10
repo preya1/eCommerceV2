07910f431f4e059da308c75b723daafdd569cc92
Albu Gabriel <albugabriel30@gmail.com>
2020-03-10 13:06:34 +0200

    Parte logare

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         alaturareButon = (Button) findViewById(R.id.buton_alaturare);
         logareButon = (Button) findViewById(R.id.buton_logare_principal);
+
+        logareButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LogareActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
