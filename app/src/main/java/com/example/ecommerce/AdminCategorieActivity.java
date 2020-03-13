7362aec1a4a5c426e97f8e82cc5fd10db2ed15ac
Albu Gabriel <albugabriel30@gmail.com>
2020-03-13 16:29:21 +0200

    Design Alaturare si Creare Cont Baza de date Inregistrare si Logare Casuta de Amintire Admin Panel Admin Categorie Design

--- app/src/main/java/com/example/ecommerce/AdminCategorieActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminCategorieActivity.java
@@ -2,13 +2,141 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AdminCategorieActivity extends AppCompatActivity {
+
+    private ImageView laptops,telefoane,ceasuri,casti;
+    private ImageView pulover,sports,tricouri,haine_femei;
+    private ImageView pantofi,ochelari,gradinarit,genti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_categorie);
+
+        laptops.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                //Trebuie sa selectam categoria specifica in noua fereastra pentru a adauga un produs sub categoria respectiva
+                intent.putExtra("categorii","laptops");
+                startActivity(intent);
+            }
+        });
+
+        telefoane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","telefoane");
+                startActivity(intent);
+            }
+        });
+
+        ceasuri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","ceasuri");
+                startActivity(intent);
+            }
+        });
+
+        casti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","casti");
+                startActivity(intent);
+            }
+        });
+
+        pulover.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","pulover");
+                startActivity(intent);
+            }
+        }));
+
+        sports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","sports");
+                startActivity(intent);
+            }
+        });
+
+        tricouri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","tricouri");
+                startActivity(intent);
+            }
+        });
+
+        haine_femei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","haine_femei");
+                startActivity(intent);
+            }
+        });
+
+        pantofi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+               Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+               intent.putExtra("categorii","pantofi");
+               startActivity(intent);
+            }
+        });
+
+        ochelari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+               Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+               intent.putExtra("categorii","ochelari");
+               startActivity(intent);
+            }
+        });
+
+        gradinarit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+               Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+               intent.putExtra("categorii","gradinarit");
+               startActivity(intent);
+            }
+        });
+
+        genti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AdminCategorieActivity.this,AdminAdaugaProdusActivity.class);
+                intent.putExtra("categorii","genti");
+            }
+        });
+
     }
 }
