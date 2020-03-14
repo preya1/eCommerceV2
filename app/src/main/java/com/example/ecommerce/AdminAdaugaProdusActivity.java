caa65f70284ec079e8408c020b31c1352eab7824
Albu Gabriel <albugabriel30@gmail.com>
2020-03-14 14:42:47 +0200

    Baza de date si Design pentru Creare Cont Baza de date Inregistrare si Logare Adaugare Imagine,Descriere,Pret a unui Produs Adaugare Data Postare Produs

--- app/src/main/java/com/example/ecommerce/AdminAdaugaProdusActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAdaugaProdusActivity.java
@@ -1,22 +1,33 @@
 package com.example.ecommerce;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class AdminAdaugaProdusActivity extends AppCompatActivity {
 
-    private String NumeCategorie;
+    private String NumeCategorie,Descriere,numeProdus,Pret,salveazaDataCurenta,salveazaOraCurenta;
     private ImageView imagineProdus;
     private Button butonProdus;
     private EditText introducereNumeProdus,introducereDescriereProdus,introducerePretProdus;
-
+    private static final int GaleriePic = 1;
+    private Uri ImagineUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +41,92 @@ public class AdminAdaugaProdusActivity extends AppCompatActivity {
         introducereDescriereProdus = (EditText)findViewById(R.id.descriere_produs);
         introducerePretProdus = (EditText)findViewById(R.id.pret_produs);
 
+        imagineProdus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                deschideGalerie();
+            }
+        });
+
+        butonProdus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                ValidareDateProdus();
+            }
+        });
+
         NumeCategorie = getIntent().getExtras().get("categorii").toString();
 
 
 
     }
+
+
+
+    private void deschideGalerie()
+    {
+        Intent intentGalerie = new Intent();
+        intentGalerie.setAction(Intent.ACTION_GET_CONTENT);
+        intentGalerie.setType("imagine/*");
+        startActivityForResult(intentGalerie,GaleriePic);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GaleriePic && resultCode == RESULT_OK && data!=null)
+        {
+               ImagineUri = data.getData();
+               imagineProdus.setImageURI(ImagineUri);
+        }
+    }
+
+    private void ValidareDateProdus()
+    {
+        Descriere = introducereDescriereProdus.getText().toString();
+        Pret = introducerePretProdus.getText().toString();
+        numeProdus = introducereNumeProdus.getText().toString();
+
+        //Daca utilizatorul nu selecteaza imaginea produsului
+        if(ImagineUri == null)
+        {
+           Toast.makeText(this,"Nu ati incarcat nici o imagine",Toast.LENGTH_LONG).show();
+        }
+        else if (TextUtils.isEmpty(Descriere))
+        {
+            Toast.makeText(this,"Va rog puneti o descriere",Toast.LENGTH_LONG).show();
+        }
+        else if (TextUtils.isEmpty(Pret))
+        {
+            Toast.makeText(this,"Nu ati stabilit un pret",Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(numeProdus))
+        {
+            Toast.makeText(this,"Nu ati introdus un nume pentru produs",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            salveazaInformatiiProdus();
+        }
+        
+
 }
+
+   //Stocheaza informatii despre produs
+
+    private void salveazaInformatiiProdus()
+    {
+        Calendar calendar = Calendar.getInstance();
+        
+        SimpleDateFormat dataCurenta = new SimpleDateFormat("MMM dd, yyyy");
+        salveazaDataCurenta = dataCurenta.format(calendar.getTime());
+
+        SimpleDateFormat dataTimp = new SimpleDateFormat("HH:mm:ss a");
+        salveazaOraCurenta = dataTimp.format(calendar.getTime());
+    }
+    }
+
