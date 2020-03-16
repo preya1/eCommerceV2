b82403c85a34a942c9dbe588e5826d2b8ced0d47
Albu Gabriel <albugabriel30@gmail.com>
2020-03-16 18:30:38 +0200

    Bara navigare Stadiu

--- app/src/main/java/com/example/ecommerce/PaginaPrincipalaActivity.java
+++ app/src/main/java/com/example/ecommerce/PaginaPrincipalaActivity.java
@@ -1,13 +1,19 @@
 package com.example.ecommerce;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,15 +22,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Map;
+import java.util.Set;
+
 public class PaginaPrincipalaActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    public SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principala);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Acasa");
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,11 +61,54 @@ public class PaginaPrincipalaActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+    private void delogare(){
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.pagina_principala, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.nav_cos)
+        {
+
+        }
+        else if(id == R.id.nav_comenzi)
+        {
+
+        }
+        else if(id == R.id.nav_categorii)
+        {
+
+        }
+        else if(id == R.id.nav_setari)
+        {
+
+        }
+        else if(id == R.id.nav_delogare)
+        {
+         delogare();
+
+        }
+
+
+
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
